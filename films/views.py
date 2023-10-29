447b3e8753e578097e91911f82588c0265cefa2f
Alok Singh <alokrpt@gmail.com>
2023-10-29 23:52:04 +0530

    basic ui without templates

---
+++ films/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    # remove these print statements later
+    print('\n\nRequest object:', request)
+    print('Request object type:', type(request), '\n\n')
+    
+    html_tags = '''
+    <h1>This is the Home Page</h1>
+    <h3>Thanks for visiting us</h3>
+    <p>MVT means:</p>
+    <ul>
+      <li>Model</li>
+      <li>View</li>
+      <li>Template</li>
+    </ul>'''
+    
+    response = HttpResponse(html_tags)
+    # remove these print statements later
+    print('Response object:', response)
+    print('Response object type:', type(response))
+    print('\n\nUser-agent info :', end='')
+    print(request.META['HTTP_USER_AGENT'], '\n\n')
+   
+    return response
+
+def main(request):
+    message = '<h1>This is the films MAIN page.</h1>'
+    return HttpResponse(message)
+def user_info(request):
+    message = '<h1>This is the films USER_INFO page.</h1>'
+    return HttpResponse(message)
