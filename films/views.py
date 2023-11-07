6e20ef6343779c8ac6a56e0ceb1ce5989b870b45
Alok Singh <alokrpt@gmail.com>
2023-11-07 22:25:18 +0530

    passed variables in html templates

--- films/views.py
+++ films/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html')
+
+
 def main(request):
-    return render(request, 'films/main.html')
+    title = 'Main Page'
+    
+    return render(request,
+                  'films/main.html',
+                  {'title': title} )
+
+
 def user_info(request):
-    return render(request, 'films/user_info.html')
+    userinfo = {
+        'username': 'Fabrício', # Put your name here
+        'country': 'Brazil', # Put your country here
+    }
+    context = {'userinfo': userinfo,
+               'title': 'User Info Page'}
+    
+    return render(request,
+                  'films/user_info.html',
+                  context)
